5a92725c89b6b4ddf0d61ebf44ee6b3e9e9e2e9c
MarilynWho <viktoriia.cheche@gmail.com>
2023-01-24 01:38:02 +0000

    adding time slots to table

--- js/index.js
+++ js/index.js
@@ -5,26 +5,34 @@ let table = $(".container");
 $("#currentDay").text(today.format("dddd, MMMM, Do"));
 
 // start of the working day
-let start = moment("18:00", "HH:mm");
+let start = moment("9:00", "HH:mm");
 // end of the working day
 let end = moment("24:00", "HH:mm");
 // table for every hour in working day
 function timeOfTheDay() {
   for (let i = start; i.diff(end); i.add(1, "h")) {
-    console.log(i.diff(end));
-    let textArea = $("<textarea>").addClass("textarea").css("flex-grow", "1"); // addClass("description")
+    // creating textarea with class textarea
+    // added 'flex-grow' attribute for it to take more spase
+    let textArea = $("<textarea>").addClass("textarea").css("flex-grow", "1");
+    // if timeblock in this hour, add class "present"
     if (i.isSame(today.startOf("hour"))) {
       textArea.addClass("present");
-    } else if (i.isAfter(today.startOf("hour"))) {
+    } else if (i.isAfter(today.startOf("hour"))) { // if timeblock is after, apply class "after"
       textArea.addClass("future");
     } else {
-      textArea.addClass("past");
+      textArea.addClass("past"); // else aplly class "past"
     }
+    // create save icon to go into button
     let icon = $("<i>").addClass("fa").addClass("fa-save");
+    // create batton and append icon inside
     let saveBttn = $("<button>").addClass("saveBtn").append(icon).css("padding", "20px");
+    // Create Timeslot with class "timeblock"
     let newTimeSlot = $("<div>").addClass("time-block").addClass("row");
+    // create container for hours
     let hour = $("<span>").addClass("hour").text(i.format("HH:mm"));
+    // append all the elements tougether
     newTimeSlot.append(hour).append(textArea).append(saveBttn);
+    // add Timeslot to table
     table.append(newTimeSlot);
   }
 }
